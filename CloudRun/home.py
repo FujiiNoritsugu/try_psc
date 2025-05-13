095d760229927a43b61c3189be81fa14398d7696
FujiiNoritsugu <quercus491@gmail.com>
2025-05-14 05:15:33 +0900

    20250514

--- CloudRun/home.py
+++ CloudRun/home.py
@@ -15,7 +15,7 @@ DEPLOYED_INDEX_ID = os.environ.get("DEPLOYED_INDEX_ID", "vs_hybridsearch_ja_depl
 
 aiplatform.init(project=PROJECT_ID, location=LOCATION)
 
-tagger = MeCab.Tagger("-r /dev/null -d /usr/local/lib/mecab/dic/unidic-lite")
+tagger = MeCab.Tagger()
 
 
 def mecab_tokenizer(text):
@@ -31,14 +31,82 @@ def mecab_tokenizer(text):
 
 vectorizer = TfidfVectorizer(tokenizer=mecab_tokenizer, token_pattern=r"(?u)\b\w\w+\b")
 
-dummy_corpus = [
-    "東京は大阪の東にある",
-    "大阪は東京の西にある",
-    "京都は大阪の北にある",
-    "札幌は東京の北にある",
-    "那覇は大阪の南にある",
+import wikipedia
+import pandas as pd
+
+# Wikipedia の言語を日本語に設定
+wikipedia.set_lang("ja")
+
+# 都道府県名のリスト
+prefectures = [
+    "北海道",
+    "青森県",
+    "岩手県",
+    "秋田県",
+    "宮城県",
+    "山形県",
+    "福島県",
+    "茨城県",
+    "栃木県",
+    "群馬県",
+    "埼玉県",
+    "千葉県",
+    "東京都",
+    "神奈川県",
+    "新潟県",
+    "富山県",
+    "石川県",
+    "福井県",
+    "山梨県",
+    "長野県",
+    "岐阜県",
+    "静岡県",
+    "愛知県",
+    "三重県",
+    "滋賀県",
+    "京都府",
+    "大阪府",
+    "兵庫県",
+    "奈良県",
+    "和歌山県",
+    "鳥取県",
+    "島根県",
+    "岡山県",
+    "広島県",
+    "山口県",
+    "徳島県",
+    "香川県",
+    "愛媛県",
+    "高知県",
+    "福岡県",
+    "佐賀県",
+    "長崎県",
+    "熊本県",
+    "大分県",
+    "宮崎県",
+    "鹿児島県",
+    "沖縄県",
 ]
-vectorizer.fit(dummy_corpus)
+
+# 各都道府県ごとに「〇〇の観光地」というタイトルの Wikipedia ページを取得
+pages = [
+    wikipedia.page(prefecture + "の観光地", auto_suggest=False)
+    for prefecture in prefectures
+]
+
+# 抽出したデータを Pandas DataFrame に格納
+df = pd.DataFrame(
+    {
+        "title": [page.title for page in pages],  # 各 Wikipedia ページのタイトル
+        "url": [page.url for page in pages],  # 各 Wikipedia ページの URL
+        "content": [page.content for page in pages],  # 各 Wikipedia ページの内容
+    }
+)
+
+# 各 Wikipedia ページの内容を corpus_ja に格納
+corpus_ja = df.content.tolist()
+
+vectorizer.fit(corpus_ja)
 
 
 def get_sparse_embedding(text):
@@ -69,17 +137,28 @@ def get_index_endpoint():
     return aiplatform.MatchingEngineIndexEndpoint(endpoint_name)
 
 
-def hybrid_search(query, top_k=5):
-    dense_embedding = get_query_dense_embedding(query)
-    sparse_embedding = get_sparse_embedding(query)
+from google.cloud.aiplatform.matching_engine.matching_engine_index_endpoint import (
+    HybridQuery,
+)
+
+
+def hybrid_search(query_text, top_k=5):
+
+    query_dense_emb = get_query_dense_embedding(query_text)
+    query_sparse_emb = get_sparse_embedding(query_text)
+    query = HybridQuery(
+        dense_embedding=query_dense_emb,
+        sparse_embedding_dimensions=query_sparse_emb["dimensions"],
+        sparse_embedding_values=query_sparse_emb["values"],
+        rrf_ranking_alpha=0.5,
+    )
 
     index_endpoint = get_index_endpoint()
 
     response = index_endpoint.find_neighbors(
         deployed_index_id=DEPLOYED_INDEX_ID,
-        queries=[dense_embedding],
+        queries=[query],
         num_neighbors=top_k,
-        sparse_queries=[sparse_embedding],
     )
 
     return response
